f5a6415b1e5e3ae4707e9edc42cace4f37550166
prmsantos1112 <prmsantosster@gmail.com>
2019-02-07 11:41:16 -0200

    SellerDaoJDBC and DaoFactory - Java

--- src/application/ProgramDaoJdbc.java
+++ src/application/ProgramDaoJdbc.java
@@ -4,6 +4,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,11 @@ public class ProgramDaoJdbc {
 		Department objectiv = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Richard Geere", "richardgeere@gmail.com", new Date(), 2880.0, objectiv);
+		
+		// Programa não conhece a Implementação; Somente a Interface;
+		// Injeção de Dependência;
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("Impressão: " + seller);
 
