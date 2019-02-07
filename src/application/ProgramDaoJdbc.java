f99fc5cf4d1c600ea13aeabf2f6c98bd43106320
prmsantos1112 <prmsantosster@gmail.com>
2019-02-06 22:31:31 -0200

    Seller Entity Class - Java

--- src/application/ProgramDaoJdbc.java
+++ src/application/ProgramDaoJdbc.java
@@ -1,14 +1,21 @@
 package application;
 
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class ProgramDaoJdbc {
 
 	public static void main(String[] args) {
 		
 		Department objectiv = new Department(1, "Books");
-		System.out.println("Impressão: " + objectiv);
+		
+		Seller seller = new Seller(21, "Richard Geere", "richardgeere@gmail.com", new Date(), 2880.0, objectiv);
+		
+		System.out.println("Impressão: " + seller);
 
 	}
-
 }
